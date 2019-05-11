2fe73ea31eb9ad962e2038a81812f78697ba49d8
kr328 <kr328@users.noreply.github.com>
2019-05-12 00:55:12 +0800

    refactor
    Former-commit-id: 4f853e7810ed0b6016898cc5ad2cbe88af18f518

--- module/src/main/cpp/inject.c
+++ module/src/main/cpp/inject.c
@@ -1,9 +1,21 @@
 #include "inject.h"
+#include "log.h"
+
+static jclass *inject_class;
+static jmethodID inject_method;
 
 void find_inject_class_method(JNIEnv *env) {
+    inject_class = (*env)->FindClass(env ,INJECT_CLASS_PATH);
+    inject_method = (*env)->FindClass(env ,INJECT_METHOD_NAME);
 
+    (*env)->ExceptionClear(env);
 }
 
 void invoke_inject_method(JNIEnv *env, const char *config_data) {
-
+    if ( inject_class ) {
+        (*env)->CallStaticVoidMethod(env, inject_class, inject_method, (*env)->NewStringUTF(env, config_data));
+    }
+    else {
+        LOGE("Failure to load dex");
+    }
 }
--- module/src/main/cpp/inject.h
+++ module/src/main/cpp/inject.h
@@ -2,5 +2,8 @@
 
 #include <jni.h>
 
+#define INJECT_CLASS_PATH "com/github/kr328/ibr/Inject"
+#define INJECT_METHOD_NAME "inject"
+
 void find_inject_class_method(JNIEnv *env);
 void invoke_inject_method(JNIEnv* env, const char *config_data);
