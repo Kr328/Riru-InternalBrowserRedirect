36109224dc678c0be55be2c8ed48b9b02e8c17b3
Kr328 <kr328@users.noreply.github.com>
2019-07-17 15:38:27 +0800

    server side

--- module/src/main/java/com/github/kr328/ibr/RuleSetMatcher.java
+++ module/src/main/java/com/github/kr328/ibr/RuleSetMatcher.java
@@ -38,6 +38,8 @@ class RuleSetMatcher {
                 continue;
             if ( !"http".equals(uri.getScheme()) && !"https".equals(uri.getScheme()) )
                 continue;
+            if ( filterUri(uri, rule) )
+                continue;
             return new Result(true, ruleSet.getTag(), rule.getTag(), uri);
         }
         return new Result();
@@ -75,6 +77,13 @@ class RuleSetMatcher {
             default:
                 return Uri.EMPTY;
         }
+    }
+
+    private static boolean filterUri(Uri uri ,Rule rule) {
+        String url = uri.toString();
+
+        return !rule.getRegexForce().matcher(url).matches() &&
+                rule.getRegexIgnore().matcher(url).matches();
     }
 
     private static <T> T optional(T o, T d) {
